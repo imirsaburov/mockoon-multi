206d33de7e81adb6bb4741ce0682beb640662211
imirsaburov <i.mirsaburov@hayotbank.uz>
2026-02-03 17:17:21 +0500

    Add humo API routes and data handling; update Docker image reference

--- envs/js/data/uzcard-data.js
+++ envs/js/data/uzcard-data.js
@@ -4,7 +4,17 @@ const CARDS = [
         expDate: "2712",
         pinfl: "12312312312312",
         phone: "+998901234567",
-        balance: 100000000
+        balance: 100000000,
+        token: "sadfasdf-asdfasdf-asdfasdf-asdf",
+        status: 0,
+        fullName: "Mocker Mockovich",
+        sms: true,
+        pincnt: "q23123123123",
+        aacct: "123123123",
+        par: "123123123123123",
+        cardtype: "private",
+        holdAmount: 0,
+        cashbackAmount: 0,
     }
 ];
 
